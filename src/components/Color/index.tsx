82daeb22d6aeed506f5515640e23a00bb74fe5f0
Denys Presich <denys.presich@gmail.com>
2023-06-29 16:57:32 +0300

    update colors

--- src/components/Color/index.tsx
+++ src/components/Color/index.tsx
@@ -17,14 +17,16 @@ export const Color: React.FC<ColorProps> = ({ hex, position }) => {
       <div className={styles.ColorBox}>
         <div className={styles.ColorBoxBg} style={{ backgroundColor: hex }} />
       </div>
-      <Form.Field name="hex">
+      <Form.Field className={styles.Field} name="hex">
+        <Form.Label className={styles.Field_Label}>Hex</Form.Label>
         <Form.Control asChild>
           <Input type="text" value={hex} />
         </Form.Control>
       </Form.Field>
-      <Form.Field name="position">
+      <Form.Field className={styles.Field} name="position">
+        <Form.Label className={styles.Field_Label}>Stop</Form.Label>
         <Form.Control asChild>
-          <Input type="number" value={position} min={0} max={100} />
+          <Input type="number" value={position.toString()} min={0} max={100} />
         </Form.Control>
       </Form.Field>
     </Form.Root>
